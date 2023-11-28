7d583b9cffb1f854882d0424649349f94b465e0c
Robert Chavana <59065215+ZalorCakeLord@users.noreply.github.com>
2023-11-28 14:27:36 -0600

    1.5 update.
    Language+Interpretor: Added 12 new characters, from 31(<) to 42(")
    Interpreter: Automatically appends the # command to the end of your code.
    Interpreter: Updates as you write your code now again!
    Interpreter: Attempting to display a character that doesn't match the built in list will now display �
    Documentation: Minor changes, added changelog, fixed $ description.
    Interpreter: Attempting to navigate to a cell higher or lower than the provided seven will cause it to wrap around to either the start or the end.
    Interpreter: Attempted fix for code comments (/) locking the page in an infinite loop.
    Interpreter: Changed the input to a resizeable textarea.

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
 
 function main() {
     const form = document.querySelector('#inputForm')
-    const input = document.querySelector('#input')
+    const input = document.getElementById('input')
     const output = document.querySelector('#output')
-    form.addEventListener('submit',(event)=>{
+	const memview = document.getElementById("memView")
+    form.addEventListener('input',(event)=>{
         event.preventDefault()
         let program = input.value
         output.innerHTML = executeCode(program)
     })
+	
+	
 }
 
 
-let alphabet = ['',"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",' ','<br>','?','!']
+
+let alphabet = ['',"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",' ','<br>','?','!',"<",">","/",".",",","+","-","*","=","(",")",'"']
 let memory = [0,0,0,0,0,0,0] //not reliant on this being exact, could make bigger but I like 7
           //  0 1 2 3 4 5 6
 //memory has seven slots
@@ -25,38 +29,52 @@ let code = ''
 let spot = 0
 
 function displayString(num){
-    return alphabet[num+1]
+    return num+1>alphabet.length?alphabet[num+1]:'�'
 }
 
-function math(op){  //needs to be rewritten, thinking #spot and the next slot in memory
-    if(op===1){
-        let out = memory[spot]+memory[spot+1]
-        return out
-    }
-    if(op===2){
-        let out = memory[spot]-memory[spot+1]
-        return out
-    }
-    if(op===3){
-        let out = memory[spot]/memory[spot+1]
-        return out
-    }
-    if(op===4){
-        let out = memory[spot]*memory[spot+1]
-        return out
-    }
-    if(op===5){
-        let out = memory[spot]%memory[spot+1]
-        return out
-    }
+function math(op){  //needs to be rewritten, thinking #spot and the next slot in memory (did this!)
+    let out;
+
+	switch (op) {
+		case 1:
+			out = memory[spot] + memory[spot + 1];
+			return out;
+			break;
+		case 2:
+			out = memory[spot] - memory[spot + 1];
+			return out;
+			break;
+		case 3:
+			out = memory[spot] / memory[spot + 1];
+			return out;
+			break;
+		case 4:
+			out = memory[spot] * memory[spot + 1];
+			return out;
+			break;
+		case 5:
+			out = memory[spot] % memory[spot + 1];
+			return out;
+			break;
+	}
+
     
     return output+=('\nerror, invalid operation')
 }
 
+function memView(){
+	let currMem = memory
+    currMem[spot] = '>'+currMem[spot]+'<'
+    output+=('<br>'+currMem.join(' ')+ ' pointer at '+(spot+1))
+	return output
+}
+
 function executeCode(code){
+	code+='#'
+	console.log(code)
     let output = ''
     let readSpace=0
-    let jump = 0
+    let jump = false
     function dump(){
         let currMem = memory
         currMem[spot] = '>'+currMem[spot]+'<'
@@ -70,12 +88,13 @@ function executeCode(code){
         if(code[i]==='/'){
             for(let x=readSpace;code[x]!='/';x++){
                 readSpace++
+				if(readSpace>code.length) break;
             }
             i=readSpace
         }
         function basic(x){
-        if(x==='>') spot++
-        if(x==='<') spot--
+        if(x==='>') spot<6?spot++:spot=0
+        if(x==='<') spot>0?spot--:spot=6
         if(x==='+') memory[spot]++
         if(x==='-') memory[spot]--
         if(x==='#') dump()
@@ -83,15 +102,7 @@ function executeCode(code){
         if(x==='*') {spot=0;memory[1]=0}
         if(x==='{') memory[spot-1] = memory[spot]
         if(x==='}') memory[spot+1] = memory[spot]
-        if(x===')') jump = readSpace
-        if(x==='(') {
-            readSpace = jump
-            i=jump
-        }
-        // if(x===',') {
-        //     if(spot === 1) memory[spot] = +prompt('operator?: ')
-        //     else memory[spot] = +prompt('input: ')
-        // }
+		
         if(x==='.') output+=(memory[spot])
         if(x==='$'){
             for(let i=0;i<memory.length;i++){
@@ -103,33 +114,9 @@ function executeCode(code){
         }
         basic(code[i])
         //conditional (how tf)
-        if(code[i]===':'){
-            let conditionalStatement = []
-            let check = 0
-            let point = 0
-            for(let j=i;!conditionalStatement.includes(';');j++){
-                readSpace++
-                conditionalStatement.push(code[j])
-                
-            }
-            for(let j=0;conditionalStatement[j]!='?';j++){
-                if(conditionalStatement[j]==='+') check++
-                if(conditionalStatement[j]==='-') check--
-                point++
-            }
-            
-             if(check===memory[spot]){
-                 for(let i=point;conditionalStatement[i]!=';';i++){
-                    if(conditionalStatement[i]===')') jump = readSpace
-                    else if(conditionalStatement[i]==='(') {
-                        readSpace = jump+1
-                    }
-                    else basic(conditionalStatement[i])
-                 }
-             }
-            i=readSpace-1
-
-        }
+        
+		
+		
         if(code[i]==='X'){
             //execute currently held instructions
             //if displaystring (memory[0]===1)
@@ -137,7 +124,7 @@ function executeCode(code){
                 spot = 0
                 let display = []
                 for(let i=2;i<=6;i++){
-                    display.push(alphabet[memory[i]])
+                    memory[i]<alphabet.length&&memory[i]>=0?display.push(alphabet[memory[i]]):display.push('�')
                 }
                 output+=(display.join(''))
                 
@@ -150,9 +137,33 @@ function executeCode(code){
             }
         }
     }
+	
+	
+function findPreviousIndexInString(str, searchString, startIndex) {
+  for (let i = startIndex - 1; i >= 0; i--) {
+    if (str[i] === searchString) {
+      return i;
+    }
+  }
+
+  // Return -1 if no match is found before the given startIndex
+  return -1;
+}
+
+function findNextIndexInString(str, searchString, startIndex) {
+  for (let i = startIndex + 1; i < str.length; i++) {
+    if (str[i] === searchString) {
+      return i;
+    }
+  }
+
+  // Return -1 if no match is found after the given startIndex
+  return -1;
+}
 
 memory = [0,0,0,0,0,0,0]
 spot=0
+console.log(output)
 return output
 }
 
@@ -187,4 +198,3 @@ return output
 
 
 window.addEventListener('DOMContentLoaded',main())
-
