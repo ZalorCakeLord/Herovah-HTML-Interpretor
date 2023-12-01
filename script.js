b8fa6d34621b1deadd115b5bdc6193cef5bb95f2
Robert Chavana <59065215+ZalorCakeLord@users.noreply.github.com>
2023-12-01 00:54:41 -0600

    Stylistic Changes
    Added the clock from my other projects because it's nifty.

--- script.js
+++ script.js
@@ -310,7 +310,17 @@ return output
 
 
 
+function time() {
+  var currentdate = new Date();
+  var datetime = (currentdate.getMonth()+1) + "/"
+      + currentdate.getDate() + "/"
+      + currentdate.getFullYear() + " @ "
+      + currentdate.getHours().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + ":"
+      + currentdate.getMinutes().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + ":"
+      + currentdate.getSeconds().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+  document.getElementById('datetime').innerHTML=datetime;}
 
+setInterval(time, 1000);
 
 
 
